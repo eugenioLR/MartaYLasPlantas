9feede256e97bb84c95eae2b0122ade74a90e3a1
Yaxeb <chejof1710@gmail.com>
2019-04-30 18:00:03 +0200

    algoritmo dificultad

--- src/MartaYLasPlantas/algoritmoDificultad.java
+++ src/MartaYLasPlantas/algoritmoDificultad.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Algoritmo;
+package MartaYLasPlantas;
 
 import java.util.ArrayList;
 
