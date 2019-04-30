c81aeabcaded103619928512ea6e4b0baf0a2581
Yaxeb <chejof1710@gmail.com>
2019-04-30 18:50:40 +0200

    dificultad 2.0 el renacer;

--- src/MartaYLasPlantas/algoritmoDificultad.java
+++ src/MartaYLasPlantas/algoritmoDificultad.java
@@ -5,8 +5,6 @@
  */
 package MartaYLasPlantas;
 
-import java.util.ArrayList;
-
 /**
  *
  * @author EDGENP
@@ -15,35 +13,23 @@ public class algoritmoDificultad {
 
     public static void main(String[] args) {
 
-        int TURNOS = 30;
+        int turnos = 30;
         int vegRest = 9;
-        int vegTablero = 0;
         double auxx;
-        ArrayList<Double> prob = new ArrayList<>();
 
-        for (int i = 0; TURNOS > i; TURNOS--) {
-            auxx = ((double) vegRest) / ((double) TURNOS);
-            System.out.println(auxx + "-" + vegRest + "-" + i);
-        //    prob.add(auxx);
-            if (auxx >= 0.5){
+        for (int i = 0; turnos > i; turnos--) {
+            auxx = ((double) vegRest) / ((double) turnos);
+            System.out.println(auxx + "-" + vegRest + "-" + turnos);
+            //    prob.add(auxx);
+            if (auxx >= 0.5) {
                 vegRest--;
+            } else if (auxx >= 0.7) {
+                vegRest -= 2;
+            } else if (auxx >= 0.85) {
+                vegRest -= 3;
+            } else if (auxx >= 1.00) {
+                vegRest -= 3 + (int) auxx / 1.00;
             }
-            
         }
-        
-
-        /* en 30 turnos han de aparecer 5 veg; es decir, un 100% si lo quieres ver así.
-        * en un turno?     30 -- 100
-        * x = 3.33333...   1  --  x
-        * en el siguiente movimiento pasa a ser:
-        *  29 -- 100
-        *  1  --  x      x = 3.448...
-        *  
-        *   30  -- 5
-        *   1   -- x   x = 0.1666666
-        *
-        *   29  -- 5
-        *   1   -- x   x = 0.172413
-         */
     }
 }
