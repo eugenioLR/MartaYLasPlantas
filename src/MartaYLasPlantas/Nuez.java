398d6478a8dd5b39f131254575fa7eef7410938b
Yaxeb <chejof1710@gmail.com>
2019-05-19 18:33:28 +0200

    Nuez

---
+++ src/MartaYLasPlantas/Nuez.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MartaYLasPlantas;
+
+/**
+ *
+ * @author EDGENP:
+ *                Eugenio Lorente
+ *                Darius Tamas
+ */
+public class Nuez extends Planta{
+    
+    /**
+     * Constructor de Nueces
+     * {@inheritDoc}
+     */
+    public Nuez(int turno) {
+        super(10, 0, turno);
+    }
+    
+}
